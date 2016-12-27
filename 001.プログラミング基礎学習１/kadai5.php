3c5b1a35bd378fb1b86d6e85b3a46d164e9cfb39
geekjob-support <geekjob_support@groove-gear.jp>
2016-12-27 13:34:34 +0900

    プログラミング基礎1の課題です！

---
+++ 001.プログラミング基礎学習１/kadai5.php
@@ -0,0 +1,19 @@
+<html>
+<head></head>
+<body>
+<?php
+//変数作成
+$param = 1;
+if ($param == 1){
+    //$paramが1の場合、ここに来ます。
+    echo '1です！';
+}elseif($param == 2){
+    //$paramが2の場合、ここに来ます。
+    echo 'プログラミングキャンプ！';
+}else{
+  //それ以外は、ここに来ます。
+    echo 'その他です！';
+}
+?>
+</body>
+</html>
