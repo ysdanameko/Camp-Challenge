0f3ac4e7ac1aa57c076c2ca5231197fdd8a46f60
geekjob-support <geekjob_support@groove-gear.jp>
2017-01-06 11:26:05 +0900

    基礎2の課題です。

---
+++ 002.プログラミング基礎学習２/kadai16.php
@@ -0,0 +1,11 @@
+<?php
+$arr=array(
+  1=>'AAA',
+  'hello'=>'world',
+  'soeda'=>'33',
+  20 => 20,
+);
+print_r($arr);
+//以下の順で、連想配列を作成してください。
+//"1"に"AAA", "hello"に"world",
+//"soeda"に"33", "20"に"20"
