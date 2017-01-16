d77967e08876857b884446ddab61de698c454b16
geekjob-support <geekjob_support@groove-gear.jp>
2017-01-16 17:42:11 +0900

    プログラミング基礎3の課題です。

---
+++ 003.プログラミング基礎学習３/kadai3_8.php
@@ -0,0 +1,5 @@
+<?php
+require 'kadai3_1.php';{}
+my_profile();
+
+//課題「ユーザー定義関数」、課題「引数1」のユーザー定義箇所を含んだutil.phpを作成し、requireで呼び出して表示する
---
+++ 003.プログラミング基礎学習３/kadai3_9.php
@@ -0,0 +1,45 @@
+<?php
+$user1 = array(
+  'id'=>'1582',
+  'name'=>'nobunaga',
+  'birth'=>'1534/5/12',
+  'add'=>'Aichi');
+$user2 = array(
+  'id'=>'1582',
+  'name'=>'nobunaga',
+  'birth'=>'1534/5/12',
+  'add'=>'Aichi');
+$user3 = array(
+  'id'=>'1582',
+  'name'=>'nobunaga',
+  'birth'=>'1534/5/12',
+  'add'=>'Aichi');
+
+$all = array(
+  'profile1'=>$user1,
+  'profile2'=>$user2,
+  'profile3'=>$user3
+);
+
+
+foreach ($all as $key => $value) {//$allを分解したよ
+  foreach ($value as $key => $value){//$valueは$user1,$user2,$user3のことで、それを分解したよ
+  if ($key == 'id' || $key == 'add'){
+    continue;
+  }
+  echo $value;
+  }
+}
+
+
+// foreach ($value as $key => $value2){
+//
+//        if($key == $name){
+//                     echo $key.':'.$value."<br>";
+//         }elseif($key == $birth){
+//           echo $key.':'.$value.'<br>';
+//         }else {
+//           continue;
+//         }
+//   }
+//3人分のプロフィールについてIDと住所以外を表示する処理を実行する。2重のforeachとcontinueを必ず用いること
